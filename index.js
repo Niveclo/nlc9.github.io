f034cf30dfe07da6e9f153a18edb18536b82509a
NLC9 <98462098+NLC9@users.noreply.github.com>
2022-07-10 22:38:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function funGames() {
+    alert("开发中，敬请期待")
+}
+function funOther() {
+    alert("开发中，敬请期待")
+}
+function funInfo() {
+    alert("此网站为作者学习阶段测试用\n内容持续更新\n联系作者：\nQQ: 3301758149\nWeChat: dMinecraftb")
+}
