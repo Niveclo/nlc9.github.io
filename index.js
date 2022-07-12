f68736bde62a73034c4581f9ddd88a91fb229a01
NLC9 <98462098+NLC9@users.noreply.github.com>
2022-07-12 16:04:17 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function funGames() {
-    alert("开发中，敬请期待")
-}
-function funOther() {
-    alert("开发中，敬请期待")
-}
-function funInfo() {
-    alert("此网站为作者学习阶段测试用\n内容持续更新\n联系作者：\nQQ: 3301758149\nWeChat: dMinecraftb")
-}
